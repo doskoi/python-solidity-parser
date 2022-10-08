766c188b741b0614c53b9af9057151b2890f259a
litcc <i@litcc.com>
2022-10-08 13:59:27 +0800

    fix: 依赖问题

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
-from solidity_parser import parse_file, objectify
+from .solidity_parser import parse, parse_file, objectify, parser, Node
 
-if __name__ == '__main__':
-    print('作为主程序运行')
-else:
-    print('package_runoob 初始化')
-    __ALL__ = ["parse_file", "objectify"]
+# if __name__ == '__main__':
+#     print('作为主程序运行')
+# else:
+#     print('package_runoob 初始化')
+__ALL__ = ["parse", "parse_file", "objectify", "parser", "Node"]
