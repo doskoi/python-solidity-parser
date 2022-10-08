643c54b85c76b85e3059196f6c2c41eb851a2127
litcc <i@litcc.com>
2022-10-08 10:31:05 +0800

    fix: init.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,7 @@
-from solidity_parser import parse_file,objectify
+from solidity_parser import parse_file, objectify
 
-__ALL__ = ["parse_file", "objectify"]
+if __name__ == '__main__':
+    print('作为主程序运行')
+else:
+    print('package_runoob 初始化')
+    __ALL__ = ["parse_file", "objectify"]
