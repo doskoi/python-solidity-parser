8e35b5eb518d4363125fb1caee87cf0328c22803
litcc <i@litcc.com>
2022-10-10 20:59:09 +0800

    fix: init.py

--- solidity_parser/__init__.py
+++ solidity_parser/__init__.py
@@ -1,3 +1,3 @@
-from solidity_parser.parser import Node, AstVisitor, parse, parse_file, visit, objectify
+from .parser import Node, AstVisitor, parse, parse_file, visit, objectify
 
 __ALL__ = ["Node", "AstVisitor", "parse", "parse_file", "visit", "objectify"]
